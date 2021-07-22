223603171f9c3db98baf11cbb76fb8fe323a200c
David <dtoepfer@csustan.edu>
2021-07-21 19:47:07 -0700

    practice and notes

---
+++ bvtca/projects/javascript-projects/practice.js
@@ -0,0 +1,43 @@
+// console logs are used to display output on the terminal
+//console.log("hello world");
+
+
+// defining a variable with const, makes it consatant. unable to change.
+
+//Javascript, runs twice, once to chack and a second to actually declare everything
+//if use a variable before it is defined with car, it shows undefined,
+// if used let, will throw an error
+//better to use let, unless absolutely needed. (not ever in this course)
+
+var MyVariable = 2;
+let MyVarianle2 = 4;
+const MyVariable3 = 7;
+
+// conditionals  === is for comparing value and type as well
+
+//falsey and truthy values
+// !! converts the variable to the boolean of its value
+//or just use boolean
+//can set variable to Boolean, number or string with <type>(<variabe>)
+
+//for numbers, all values are truthy except 0 
+//all strings are truthy, except for an empty string
+//for arrays, it is always truthy, if it's defines, it's truthy.
+//undefined is also falsey
+//null is falsey
+
+
+//to check the type typeof(<variable>);
+
+//for strings can use '' or "" or ``
+// putting a \ before a command, tells the compiler that it is to be taken as a 
+// character, not a command
+//ex.. \" will print ", not use it as a functional
+
+
+
+//with less than and greater than on strings, it compares strings alphabettically 
+
+
+
+//calling variable in middle of string use ${} or " + "
