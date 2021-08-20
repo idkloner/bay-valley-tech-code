79fc46611291d07c44ade148e32685bd2f52bcf1
David <dtoepfer@csustan.edu>
2021-08-20 16:33:09 -0700

    oop

---
+++ bvtca/projects/javascript-projects/OOP/app.js
@@ -0,0 +1,90 @@
+//Book constructor
+function Book(title, author,isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+//UI constructor
+function UI(){}
+
+//add book to list
+UI.prototype.addBookToList = function(book){
+    const list = document.getElementById('book-list');
+    //create tr element
+    const row = document.createElement('tr');
+    // insert cols
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href = "#" class = "delete">X</a></td>
+    `;
+    
+    list.appendChild(row);
+}
+
+//show alert
+UI.prototype.showAlert = function(message, className){
+    //create div
+    const div = document.createElement('div');
+    //add classes
+    div.className = `alert ${className}`;
+    //add text
+    div.appendChild(document.createTextNode(message));
+    //get parent
+    const container = document.querySelector('.container');
+    //get form
+    const form = document.querySelector('#book-form');
+    //insert alert
+    container.insertBefore(div, form);
+
+    //timeout 3 sec
+    setTimeout(function(){
+        document.querySelector('.alert').remove();
+    }, 3000);
+    
+}
+
+
+//clear fields
+UI.prototype.clearFields = function() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+}
+
+
+//Event Listeners
+document.getElementById('book-form').addEventListener('submit', 
+function(e){
+    //Get form values
+    const title = document.getElementById('title').value,
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('isbn').value
+
+    //Instantiate book
+    const book = new Book(title, author, isbn);
+   
+    //Instantiate UI
+    const ui = new UI();
+
+    //Validate
+    if(title === '' || author === '' || isbn === ''){
+        // error alert
+        ui.showAlert('Please check fields', 'error');
+    } else {
+    //add book to list
+    ui.addBookToList(book);
+
+    //show success
+    ui.showAlert('Book added', 'success');
+
+    //clear fields
+    ui.clearFields()
+
+    }
+
+
+    e.preventDefault();
+});
