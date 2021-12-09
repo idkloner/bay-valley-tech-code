157ac8798e0a5b5003fa441d369567751bb80711
David <dtoepfer@csustan.edu>
2021-12-08 16:35:02 -0800

    halfway update

--- bvtca/projects/angular-projects/routing-start/src/app/servers/servers.component.ts
+++ bvtca/projects/angular-projects/routing-start/src/app/servers/servers.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -11,14 +11,15 @@ export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
 
   constructor(private serversService: ServersService,
-              private router: Router) { }
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
 
   onReload(){ 
-    this.router.navigate(['/servers']);
+    //this.router.navigate(['/servers'], {relativeTo: this.route});
   }
 
 }
