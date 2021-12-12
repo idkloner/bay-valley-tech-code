206e61af69888da7c9740e3e55c5a50e952a0709
David <dtoepfer@csustan.edu>
2021-12-12 15:37:10 -0800

    after accidental deletion and restoration

---
+++ bvtca/projects/angular-projects/DToepfer-journal/src/app/journals/journal-list/journal-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { JournalService } from 'src/app/journals/journal.service';
+import { journals } from 'src/app/mock.journal';
+
+@Component({
+  selector: 'app-journal-list',
+  templateUrl: './journal-list.component.html',
+  styleUrls: ['./journal-list.component.css']
+})
+export class JournalListComponent implements OnInit {
+
+  //id: number;
+  
+
+  constructor(private journalService: JournalService,
+              private route: ActivatedRoute,
+              private router: Router) {
+
+              }
+
+
+
+  ngOnInit(): void{
+    // this.route.params
+    //   .subscribe(
+    //     (params: Params) => {
+    //       this.id = +params['id'];
+    //       this.recipe = this.recipeService.getRecipe(this.id);
+    //   }
+    // );
+  }
+
+//   onEditEntry() {
+//     this.router.navigate(['edit'], {relativeTo: this.route});
+
+// }
+
+}
