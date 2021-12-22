992886ecc8ee9667ea66bb9c7fffb439c950a199
David <dtoepfer@csustan.edu>
2021-12-21 17:18:28 -0800

    really done

--- bvtca/projects/angular-projects/obs-01-start/src/app/home/home.component.ts
+++ bvtca/projects/angular-projects/obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,13 +12,41 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.firstObsSubsctibtion = interval(1000).subscribe(count => {
-      console.log(count);
+    // this.firstObsSubsctibtion = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+    const customIntervalObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);      //built in method to observables, gives next value
+        if(count == 2) {
+          observer.complete();  //completes the observable before the error, cause error kills the observable but does not complete
+        }
+        if (count > 3) {
+          observer.error(new Error('count is greater than 3')); //creating our own error for 
+        }
+        count++;
+      }, 1000);
+    });
+
+    ; //for operartors
+
+    this.firstObsSubsctibtion = customIntervalObservable.pipe(filter(data => {
+      return data > 0;                //filter must be true or false value, so true when data > 0 i.e. will return starting at round: 2
+    }),map((data: number) =>{         //pipe injects the map operater which allows us to change the data
+      return 'Round: ' + (data + 1);  //this modifies the data before it is logged 
+    })).subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
+      alert(error.message);           //this is error handeling logs it and alerts instead of red thrown error
+    }, () => {                        // the () is empty cause when copmpleted nothing is passed
+      console.log('completed');       //here you can do any necessary clean up, similar to onDestroy
     });
   }
-
     ngOnDestroy(){
       this.firstObsSubsctibtion.unsubscribe();
+      
     }
   }
 
---
+++ bvtca/projects/angular-projects/obs-01-start/src/app/user.service.ts
@@ -0,0 +1,13 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+
+@Injectable({providedIn: 'root'})           //providedin root, is a shortcut so we dont need to delcare it in the provided in app.module
+export class userService{
+    // activatedEmitter = new EventEmitter<boolean>();  //old way
+    activatedEmitter = new Subject<boolean>();  //subject is special observable, 
+    //a more active one, 
+    //only difference coding wise is when calling, use next not emit, 
+    //dont use it when using @output, butotherwise recomended
+    //when you are doing cross component communication through services,
+    //and manually call next/emit, and manually subscribe.
+}
