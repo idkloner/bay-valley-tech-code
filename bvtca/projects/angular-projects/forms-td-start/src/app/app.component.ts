55996d57f7cf78ec46683fafe6e6f97963607706
David <dtoepfer@csustan.edu>
2022-01-05 16:18:54 -0800

    finished

--- bvtca/projects/angular-projects/forms-td-start/src/app/app.component.ts
+++ bvtca/projects/angular-projects/forms-td-start/src/app/app.component.ts
@@ -8,9 +8,36 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = "pet";
+  answer = "";
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted= false;
+
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: '',
+    //   },
+    //   secret: "pet",
+    //   questionAnswer: "",
+    //   gender: "male"
+
+    // });
+    this.signupForm.form.patchValue({    //patchvalue to only override a part of the form
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm) {   //this way requires passing f on html
@@ -19,5 +46,15 @@ export class AppComponent {
 
   onSubmit(){
   console.log(this.signupForm);     //this way does not require passing f
+  this.submitted = true;
+  this.user.username = this.signupForm.value.userData.username;
+  this.user.email = this.signupForm.value.userData.email;
+  this.user.secretQuestion = this.signupForm.value.secret;
+  this.user.answer = this.signupForm.value.questionAnswer;
+  this.user.gender = this.signupForm.value.gender;
+
+
+  this.signupForm.reset();
+
   }
 }
