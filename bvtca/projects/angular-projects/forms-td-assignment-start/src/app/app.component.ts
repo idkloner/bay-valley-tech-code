16a5bb1ca0092f3db549d52624bf8e5432be9fb5
David <dtoepfer@csustan.edu>
2022-01-13 15:54:02 -0800

    finished assignment

--- bvtca/projects/angular-projects/forms-td-assignment-start/src/app/app.component.ts
+++ bvtca/projects/angular-projects/forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('f') signupForm: NgForm;
+  defaultSub = "advanced";
+  password = "";
+  user = {
+    email: "",
+    subscription: "",
+    password: ""
+
+  };
+  submitted = false;
+ 
+  
+onSubmit(){
+  console.log(this.signupForm);
+  this.submitted = true;
+  this.user.email = this.signupForm.value.userData.email;
+  this.user.subscription = this.signupForm.value.subscription;
+  this.user.password = this.signupForm.value.password;
+}
 
 }
