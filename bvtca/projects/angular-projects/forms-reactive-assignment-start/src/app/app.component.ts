1a4047e5c3e6c5b07ae21585a42f88cf91b69d22
David <dtoepfer@csustan.edu>
2022-01-19 16:22:09 -0800

    main framework done, with some backend

--- bvtca/projects/angular-projects/forms-reactive-assignment-start/src/app/app.component.ts
+++ bvtca/projects/angular-projects/forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  projectForm: FormGroup;
+  forbiddenProjectNames = ['Test'];
+  defaultStatus = "stable";
+
+  ngOnInit(){
+    this.projectForm = new FormGroup({
+      'userData': new FormGroup({
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'name': new FormControl(null,[Validators.required, , this.forbiddenNames.bind(this)])
+      }),
+      'status': new FormControl('stable', [Validators.required])
+    });
+  }
+
+  onSubmit(){
+    this.projectForm.reset();
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenProjectNames.indexOf(control.value) !== 1){
+      return({'nameisForbidden': true});
+    }
+    return null;
+  }
+
+
 }
