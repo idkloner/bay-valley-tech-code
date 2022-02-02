71a8d8a4beb5a9e1582ca5e7b5c767e83d875bf9
David <dtoepfer@csustan.edu>
2022-02-01 19:43:01 -0800

    fixed form error

--- bvtca/projects/angular-projects/udemy-course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ bvtca/projects/angular-projects/udemy-course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -88,8 +88,6 @@ export class RecipeEditComponent implements OnInit {
         }
       }
     }
-  
-
     this.recipeForm = new FormGroup({
       'name': new FormControl(recipeName, Validators.required),
       'imagePath': new FormControl(recipeImagePath, Validators.required),
