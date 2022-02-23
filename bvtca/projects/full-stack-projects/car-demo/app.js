d9ce46cf56e245836970f97b5cc05224a4e1ba60
David <dtoepfer@csustan.edu>
2022-02-22 19:40:09 -0800

    auth proj start

--- bvtca/projects/full-stack-projects/car-demo/app.js
+++ bvtca/projects/full-stack-projects/car-demo/app.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const jsonwebtoken = require('jsonwebtoken');
 const mysql = require('mysql2/promise');
 const app = express();
-const port = 3000;
+const port = 3306;
 
 app.use(express.json());
 
-app.use(async (req, res, next) => {
+const JWT_KEY = "THIS_IS_TOP_SECRET";
+
+app.use(async (req, res, next) => {   //match login
   global.db = await mysql.createConnection({ 
     host: 'localhost', 
     user: 'root', 
-    password: 'password', 
+    password: 'Anch0r07', 
     database: 'bvt_demo', 
     multipleStatements: true 
   });
@@ -17,6 +20,27 @@ app.use(async (req, res, next) => {
   global.db.query(`SET time_zone = '-8:00'`);
   await next();
 });
+
+
+app.post('/login', async (req, res) => {
+  console.log('login.req.body', req.body);
+
+  const  { email, password } = req.body;
+  
+  const [[user]] = await global.db.query('SELECT * FROM user WHERE email = ? AND password = ?', 
+  [email, password])
+
+  if (user) {
+    const token = jsonwebtoken.sign({ id: user.id, email: user.email }, JWT_KEY);
+    
+    res.json({
+      jwt: token
+    });
+  } else {
+    res.send('Username or password incorrect');
+  }
+});
+
 
 app.get('/:id', async (req, res) => {
   const [data] = await global.db.query(`SELEcT * FROM car WHERE id = ?`, [req.params.id]);
