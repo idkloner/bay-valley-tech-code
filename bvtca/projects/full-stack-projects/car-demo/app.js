6162eed31e729972943e20a0f380199c4a82cc24
David <dtoepfer@csustan.edu>
2022-02-24 19:10:59 -0800

    finished required

--- bvtca/projects/full-stack-projects/car-demo/app.js
+++ bvtca/projects/full-stack-projects/car-demo/app.js
@@ -52,7 +52,7 @@ app.post('/login', async (req, res) => {
 
   if (user) {
     const token = jsonwebtoken.sign({ id: user.id, email: user.email }, JWT_KEY);
-    
+  
     res.json({
       jwt: token
     });
@@ -62,7 +62,12 @@ app.post('/login', async (req, res) => {
 });
 
 app.get('/', authenticateJWT, async (req, res) => {
-  const[data] = await global.db.query('SELECT * FROM car');
+
+  const authHeader = req.headers.authorization;
+  const token = authHeader.split(' ')[1];
+  //console.log(req);
+  const[data] = await global.db.query('SELECT * FROM car Where user_id = ?', 
+  [token]);
 
   res.send({
     data
@@ -78,10 +83,36 @@ app.get('/:id', async (req, res) => {
   });
 });
 
-app.post('/',  async (req, res) => {
-  await global.db.query(`INSERT INTO car (make, color) VALUES (?, ?)`, [
-    req.body.makeId, 
-    req.body.color
+app.post('/new_car', authenticateJWT,  async (req, res) => {
+  
+  const  { make, color} = req.body;
+  console.log("req.body = " + req.body);
+  
+  const authHeader = req.headers.authorization;
+  const token = authHeader.split(' ')[1];
+
+  //console.log(make, color);
+  await global.db.query(`INSERT INTO car (make, color, date_entered, user_id) VALUES (?, ?, now(), ?)`, [
+    req.body.make, 
+    req.body.color,
+    
+    token
+  
+    
+  ]);
+
+  res.send('I am posting data!')
+});
+
+
+app.post('/new_user',  async (req, res) => {
+  const  { email, password } = req.body;
+  console.log(req.body)
+  //console.log(make, color);
+
+  await global.db.query(`INSERT INTO user (email, password) VALUES (?, ?)`, [
+    req.body.email, 
+    req.body.password
   ]);
 
   res.send('I am posting data!')
@@ -89,6 +120,7 @@ app.post('/',  async (req, res) => {
 
 app.put('/', (req, res) => {
   // You can pretty much do the same thing with POSTs
+  
 });
 
 app.delete('/:id', async (req, res) => {
@@ -99,3 +131,4 @@ app.delete('/:id', async (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
+
