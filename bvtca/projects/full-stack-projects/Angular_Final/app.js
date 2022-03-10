89e444adb1abf6cac06eb5c439c177514c6f94fa
David <dtoepfer@csustan.edu>
2022-03-10 12:22:57 -0800

    fixed add journal

--- bvtca/projects/full-stack-projects/Angular_Final/app.js
+++ bvtca/projects/full-stack-projects/Angular_Final/app.js
@@ -5,7 +5,12 @@ const app = express();
 const port = 3000;
 const crypto = require('crypto');
 
-app.use(express.json());
+//app.use(express.json());
+app.use(express.json(), function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const JWT_KEY = "THIS_IS_TOP_SECRET";
 
@@ -72,55 +77,37 @@ app.post('/login', async (req, res) => {
 
  app.get('/',  async (req, res) => {
 
-   const authHeader = req.headers.authorization;
+   //const authHeader = req.headers.authorization;
   
    //console.log(req);
    const[data] = await global.db.query( "SELECT date, entry FROM entrys");
-
+  
    res.send({
-     data
+   data
    });
  });
 
-// app.get('/brands',  async (req, res) => {
 
-//   //console.log(req);
-//   const[data] = await global.db.query("SELECT * from product_brands");
-
-//   res.send({
-//     data
-//   });
-// });
-
-// app.get('/categories',  async (req, res) => {
-  
-//   //console.log(req);
-//   const[data] = await global.db.query("SELECT * from product_categories");
-
-//   res.send({
-//     data
-//   });
-// });
 
 
 app.get('/:id', async (req, res) => {
-  const [data] = await global.db.query(`SELECT * FROM products WHERE id = ?`, [req.params.id]);
+  const [data] = await global.db.query(`SELECT * FROM entrys WHERE id = ?`, [req.params.id]);
 
   res.send({
     data
   });
 });
 
+
 app.post('/new',  async (req, res) => {
   
-  const  { id, entry } = req.body;
+  const  { entry } = req.body;
   console.log("req.body = " + req.body);
   
   //const authHeader = req.headers.authorization;
   //const token = authHeader.split(' ')[1];
 
-  await global.db.query('Insert into entrys values (?, now(), ?)', [
-    req.body.id,
+  await global.db.query('Insert into entrys (date, entry) values (now(), ?)', [
     req.body.entry
   
   ]);
